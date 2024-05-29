f218181c2bb8c9e4e49e0bf819fcde190add1ba3
Ionut Elian TUDOR <ionuteliantudor@gmail.com>
2024-05-29 23:52:30 +0300

    Attache volume

--- teamcity_ec2_instance.tf
+++ teamcity_ec2_instance.tf
@@ -2,10 +2,11 @@
   # Create Ec2 Instance called teamcity 
 **********************************************************/
 resource "aws_instance" "team_city_instance" {
-  ami           = var.ami_id
-  instance_type = var.teamcity_instance_type
-  key_name      = aws_key_pair.teamcity_key.key_name
-  count         = var.teamcity_instance_count
+  ami               = var.ami_id
+  instance_type     = var.teamcity_instance_type
+  key_name          = aws_key_pair.teamcity_key.key_name
+  count             = var.teamcity_instance_count
+  availability_zone = "eu-central-1a"
 
   user_data = file("scripts/teamcity_entry_script.sh")
 }
