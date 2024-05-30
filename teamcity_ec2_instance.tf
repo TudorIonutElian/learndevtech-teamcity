07d651ed6dbdb4232e8aff75cd49c51531494d75
Ionut Elian TUDOR <ionuteliantudor@gmail.com>
2024-05-30 18:56:22 +0300

    New fix of the AMI

--- teamcity_ec2_instance.tf
+++ teamcity_ec2_instance.tf
@@ -2,7 +2,7 @@
   # Create Ec2 Instance called teamcity 
 **********************************************************/
 resource "aws_instance" "team_city_instance" {
-  ami           = data.aws_ami.ami_id.id
+  ami           = var.ami_id
   instance_type = var.teamcity_instance_type
   key_name      = aws_key_pair.teamcity_key.key_name
   count         = var.teamcity_instance_count
